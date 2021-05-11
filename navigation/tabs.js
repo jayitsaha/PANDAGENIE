805855207dabbe7aad4b96e1b84a60a19805c453
Jayit Saha <jayit.saha2018@vitstudent.ac.in>
2021-05-12 04:11:30 +0530

    PANDAGENIE Bottom Tab Navigator whole other level customize -- FEAT

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -63,6 +63,12 @@ const Tabs = () => {
     <Tab.Navigator
       tabBarOptions={{
         showLabel: false,
+        style: {
+          height: 60,
+          borderTopWidth: 0,
+          backgroundColor: 'transparent',
+          elevation: 0,
+        },
       }}>
       <Tab.Screen
         name="Home"
@@ -71,6 +77,63 @@ const Tabs = () => {
           tabBarIcon: ({focused}) => (
             <Image
               source={icons.cutlery}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
+        }}
+      />
+
+      <Tab.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.search}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
+        }}
+      />
+
+      <Tab.Screen
+        name="Like"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.like}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+          tabBarButton: props => <TabBarCustomButton {...props} />,
+        }}
+      />
+
+      <Tab.Screen
+        name="User"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.user}
               resizeMode="contain"
               style={{
                 width: 25,
