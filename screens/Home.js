a99156fdad11830531879c44537c2c2ebe5d1a0f
Jayit Saha <jayit.saha2018@vitstudent.ac.in>
2021-05-12 20:51:24 +0530

    PANDAGENIE Home Screen Safe Area View and Styles init

--- screens/Home.js
+++ screens/Home.js
@@ -327,10 +327,27 @@ const Home = () => {
   ];
 
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* {renderHeader()} */}
+      {/* {renderMainCategories()}
+      {renderRestaurantList()} */}
+    </SafeAreaView>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 1,
+  },
+});
 export default Home;
