18064cb1d2f33799ae1f0033560ad0affe3ab529
Jayit Saha <jayit.saha2018@vitstudent.ac.in>
2021-05-13 03:38:04 +0530

    PANDAGENIE Restaurant Detail Screen Header

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -13,10 +13,74 @@ import {isIphoneX} from 'react-native-iphone-x-helper';
 import {icons, COLORS, SIZES, FONTS} from '../constants';
 
 const Restaurant = ({route, navigation}) => {
+  const scrollX = new Animated.Value(0);
+  const [restaurant, setRestaurant] = React.useState(null);
+  const [orderItems, setOrderItems] = React.useState([]);
+  const [currentLocation, setCurrentLocation] = React.useState(null);
+
+  function renderHeader() {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingLeft: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}
+          onPress={() => navigation.goBack()}>
+          <Image
+            source={icons.back}
+            resizeMode="contain"
+            style={{
+              width: 30,
+              height: 30,
+            }}
+          />
+        </TouchableOpacity>
+
+        {/* Restaurant Name */}
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View
+            style={{
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+              paddingHorizontal: SIZES.padding * 3,
+              borderRadius: SIZES.radius,
+              backgroundColor: COLORS.lightGray3,
+            }}>
+            <Text style={{...FONTS.h3}}>{restaurant?.name}</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            width: 50,
+            paddingRight: SIZES.padding * 2,
+            justifyContent: 'center',
+          }}
+          onPress={() => console.log('List')}>
+          <Image
+            source={icons.list}
+            resizeMode="contain"
+            style={{
+              width: 30,
+              height: 30,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <Text>HI</Text>
-      {/* {renderHeader()} */}
+      {/* <Text>HI</Text> */}
+      {renderHeader()}
       {/* {renderFoodInfo()} */}
       {/* {renderOrder()} */}
     </SafeAreaView>
