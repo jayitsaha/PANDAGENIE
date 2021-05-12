b5d31d48694488ec6d2918a26fe65a500d7a3a36
Jayit Saha <jayit.saha2018@vitstudent.ac.in>
2021-05-13 03:35:34 +0530

    PANDAGENIE Restaurant Detail Screen Base Styles Init

--- screens/Restaurant.js
+++ screens/Restaurant.js
@@ -22,5 +22,23 @@ const Restaurant = ({route, navigation}) => {
     </SafeAreaView>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray2,
+  },
+  dotsRootContainer: {
+    height: 30,
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: SIZES.padding,
+  },
+  dot: {
+    borderRadius: SIZES.radius,
+    marginHorizontal: 6,
+  },
+});
 export default Restaurant;
